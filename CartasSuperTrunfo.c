6b6ba996a03067a7f1fd74e83553faba97de6bfa
gitpremiumflip <contatocomfelipess@gmail.com>
2025-02-23 14:53:02 +0000

    inicio do codigo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,10 +9,18 @@
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
+    char inicialestado1, inicialestado2;
+    char idcard01[4], idcard02[4];
+    char idcidade01[50], idcidade02[50];
+    int popestado01, popestado02;
+    float areakm01, areakm02;
+    float pibest01, pibest02;
+    int pontur01, pontur02;
+        
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    
     
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
